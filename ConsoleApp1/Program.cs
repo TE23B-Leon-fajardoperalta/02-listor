097bab9b1be332c9556de50b7838bbbb1918e3c5
Leone <leon.fajardoperalta@elev.ga.ntig.se>
2024-11-13 20:44:29 +0100

    gitignore

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿// List<string> dog = ["dog1", "dog2", "dog3", "dog4", "dog5"];
+// //_______________________________________________________________
+
+// List<string> dog = ["dog1", "dog2", "dog3", "dog4", "dog5"];
+// for ( int i = 0; i < 5; i++)
+// {
+//     Console.WriteLine(dog[i]);
+
+// }
+//     Console.ReadLine();
+// //____________________________________________________________________
+// List<int> dog = [1, 2, 3, 4, 5];
+// //____________________________________________________________________
+// List<string> dog = ["dog1", "dog2", "dog3", "dog4", "dog5"];
+// List<int> cog = [1, 2, 3, 4, 5];
+
+// for (int i = 0; i < 5; i++)
+// {
+//     Console.WriteLine(dog[i] + " har fått betyget " + cog[i]);
+// }
+// Console.ReadLine();
+// //_____________________________________________________________________
+
+// List<string> dog = [];
+// string exit = Console.ReadLine();
+
+// while (exit != "exit")
+// {
+//     dog.Add(exit);
+//     exit = Console.ReadLine();
+// }
+// Console.ReadLine();
+// //_____________________________________________________________________
+// List<string> cities = [];
+// string exit = Console.ReadLine();
+
+// while (exit != "exit")
+// {
+
+//     cities.Add(exit);
+//     exit = Console.ReadLine();    
+// }
+
+// for (int i = 0; i < cities.Count ; i++)
+// {
+//     Console.WriteLine(cities[i]);
+// }
+// Console.ReadLine();
+// //______________________________________________________________________
+
